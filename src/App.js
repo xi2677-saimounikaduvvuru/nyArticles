5996704925bb8b172df25d2a85fc47a84a9fd659
SaiMounika Duvvuru <SaiMounika.Duvvuru@XEBIAINDIA.LOCAL>
2022-04-25 22:19:05 +0530

    most viewed articles project

--- src/App.js
+++ src/App.js
@@ -1,24 +1,41 @@
-import logo from './logo.svg';
 import './App.css';
+import React, {useState, useEffect} from 'react';
 
 function App() {
+
+  const [articles, setArticles] = useState([]);
+  const [selectedArticle, setSelectedArticle] = useState(null);
+
+  useEffect(() => {
+    const fetchArticles = async () => {
+    try {
+        const res = await fetch (`https://api.nytimes.com/svc/mostpopular/v2/viewed/1.json?api-key=l4xA9o6wx17qGbAL9gKYkn2UrwdjVWAF`)
+        const articles = await res.json();
+        console.log(articles);
+        setArticles(articles.results);
+        console.log(articles.results);
+      } catch (error) {
+        console.error(error);
+      }
+    }
+    fetchArticles();
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <section>
+        {articles.map((article) => {
+            const {title, abstract, byline, updated, url} = article;
+            return (
+              <article key={article.id}>
+                <a href={article.url} target="_blank"><h2>{article.title}</h2></a>
+                <span>{article.abstract}</span>
+                <h6>{article.updated}</h6>
+              </article> 
+            )
+        })}
+      </section>
+    </>
   );
 }
 
